d97fc0f625f6ba0da2294bcd4f6d32a2537119ff
prologue <prologue@prologuesMac.local>
2017-06-24 19:55:33 +0900

    Add Extension With UITableView

--- SampleSourceCode/ViewController.swift
+++ SampleSourceCode/ViewController.swift
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+}
 
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell();
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
 }
 
